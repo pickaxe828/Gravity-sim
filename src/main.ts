6cbd2d52e90a530226d537050188e23e08dc20e3
Pickaxe828 <joshuajcn828@gmail.com>
2023-03-21 00:54:14 +0800

    ✨ Update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import 'virtual:windi.css'
+// Import our custom CSS
+import './styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 import p5 from "p5"
 import { Entity } from "./utils/entity"
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import WindiCSS from 'vite-plugin-windicss'
+const path = require('path')
 
 export default {
-    plugins: [
-        WindiCSS(),
-    ],
+    root: path.resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+    server: {
+        port: 8080,
+        hot: true
+    }
 }
